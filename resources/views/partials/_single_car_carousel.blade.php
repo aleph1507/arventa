be2a4f2d421c28f82a7e557f86d9a0926bd0014c
xrristo <xrristo@gmail.com>
2018-10-16 23:38:12 +0200

    asdasd

--- resources/views/partials/_single_car_carousel.blade.php
+++ resources/views/partials/_single_car_carousel.blade.php
@@ -1,12 +1,36 @@
+<?php
+  $gals = explode(';', $car->galleryImages);
+  array_pop($gals);
+  // print_r($gals);
+  // array_unshift($gals, $car->featuredImage);
+  $ac = 0;
+?>
+
+<div id="singleCarCarousel">
+  <ul id="imageGallery">
+    @foreach($gals as $gal)
+      <li data-thumb="{{asset('images/cars/' . $car->id . '/' . 'gallery/' . $gal)}}"
+          data-src="{{asset('images/cars/' . $car->id . '/' . 'gallery/' . $gal)}}"
+      >
+        <img src="{{asset('images/cars/' . $car->id . '/' . 'gallery/' . $gal)}}" alt="{{$car->model}}">
+      </li>
+      {{-- <div class="carousel-item {{$ac == 0 ? 'active' : ''}}">
+        <img src="{{asset('images/cars/' . $car->id . '/' . 'gallery/' . $gal)}}"
+         alt="{{$car->model}}" class="d-block w-100">
+      </div> --}}
+      <?php $ac++; ?>
+    @endforeach
+    {{-- <li data-thumb="img/thumb/cS-1.jpg" data-src="img/largeImage.jpg">
+      <img src="img/cS-1.jpg" />
+    </li>
+    <li data-thumb="img/thumb/cS-2.jpg" data-src="img/largeImage1.jpg">
+      <img src="img/cS-2.jpg" />
+    </li> --}}
+  </ul>
+</div>
+
 <div id="singleCarCarousel" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <?php
-      $gals = explode(';', $car->galleryImages);
-      array_pop($gals);
-      // print_r($gals);
-      // array_unshift($gals, $car->featuredImage);
-      $ac = 0;
-    ?>
 
     {{-- <div class="carousel-item active">
       <img src="{{asset('images/cars/' . $car->id . '/' . $car->featuredImage)}}"
@@ -29,12 +53,13 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-
-<div id="carousel-status" class="carousel-static row spacing-top thumb-gallery">
-  @foreach($gals as $gal)
-    <div class="col-xs-3 col-md-3 sm-spacing">
-      <img src="{{asset('images/cars/' . $car->id . '/' . 'gallery/' . $gal)}}"
-       alt="{{$car->model}}" class="d-none d-md-block w-100 img-fluid">
-    </div>
-  @endforeach
-</div>
+{{-- <div class="container">
+  <div id="carousel-status" style="margin:auto;" class="text-center carousel-static row spacing-top thumb-gallery">
+    @foreach($gals as $gal)
+      <div class="col-xs-6 col-md-4 sm-spacing">
+        <img src="{{asset('images/cars/' . $car->id . '/' . 'gallery/' . $gal)}}"
+        alt="{{$car->model}}" class="d-none d-md-block w-100 img-fluid">
+      </div>
+    @endforeach
+  </div>
+</div> --}}
