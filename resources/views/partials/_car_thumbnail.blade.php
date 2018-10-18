a6de27173d9169f75a7e86927e84213c07ee10ab
xrristo <xrristo@gmail.com>
2018-10-18 23:44:48 +0200

    listing0001

--- resources/views/partials/_car_thumbnail.blade.php
+++ resources/views/partials/_car_thumbnail.blade.php
@@ -3,9 +3,9 @@
     {{-- {{var_dump($car)}} --}}
     <img src="{{asset('images/cars/' . $car->id . '/' . $car->featuredImage)}}" class="car-thumb-img img-fluid">
     <h3 class="car-thumb-header">{{$car->brand['name']}}</h3>
-    <p class="lead">{{$car->model}}</p>
-    <span class="car-thumb-info">Status: <span class="status">-</span></span>
-    <span class="car-thumb-info">Ref nr. <span class="refnr">{{$car->refNr}}</span></span>
-    <div class="discount">save 15%</div>
+    <p class="lead" style="margin-bottom:1%;">{{$car->model}}</p>
+    <span class="car-thumb-info">Status: <span class="status">{{$car->status}}</span></span>
+    <span class="car-thumb-info">Ref nr. <span class="refnr">{{$car->VIN}}</span></span>
+    <div class="discount">save {{$car->discountPercent}}%</div>
   </div>
 </a>
